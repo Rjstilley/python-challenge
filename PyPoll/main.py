94ad5a7525611a67cc5780252239805adc861787
Beccapwd <rjstilley@gmail.com>
2021-02-09 22:17:05 -0600

    Print and write out election results

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,8 +13,8 @@ CandAll = []
 
 # # make variables
 vote_count = 0
-vote_perc = 0
-cand_list = {"Candidate": [], "Votes": [], "Vote Percent": []}
+vote_perc = 0.0
+cand_list = {}
 winner = 0
 count = 0
 
@@ -25,9 +25,42 @@ with open(PyPollcsv, newline="") as csvfile:
 
     for A in csvreader:
 
-        count = count + 1
-        CandAll.append(A[2])
-        if A[2] not in cand_list["Candidate"]:
-            cand_list["Candidate"].append(A[2])
-    for i in cand_list:
-        print(f"{cand_list[i]}: {vote_perc}% ")
+        # count = count + 1
+        # CandAll.append(A[2])
+        if A[2] in cand_list.keys():
+            cand_list[A[2]] += 1
+        else:
+            cand_list[A[2]] = 1
+
+
+vote_count = sum(cand_list.values())
+
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {vote_count}")
+print("-------------------------")
+
+for C in cand_list:
+    vote_perc = round((cand_list[C] * 100) / vote_count, 3)
+    print(f"{C}: {vote_perc}% ({cand_list[C]})")
+    if winner < cand_list[C]:
+        winner = cand_list[C]
+        Elected = C
+print("-------------------------")
+print(f"Winner: {Elected}")
+print("-------------------------")
+
+with open(writeFile, 'w') as outputFile:
+    outputFile.write("Election Results\n")
+    outputFile.write("-------------------------\n")
+    outputFile.write(f"Total Votes: {vote_count}\n")
+    outputFile.write("-------------------------\n")
+    for C in cand_list:
+        vote_perc = round((cand_list[C] * 100) / vote_count, 3)
+        outputFile.write(f"{C}: {vote_perc}% ({cand_list[C]})\n")
+    # if winner < cand_list[C]:
+    #     winner = cand_list[C]
+    #     Elected = C
+    outputFile.write("-------------------------\n")
+    outputFile.write(f"Winner: {Elected}\n")
+    outputFile.write("-------------------------")
